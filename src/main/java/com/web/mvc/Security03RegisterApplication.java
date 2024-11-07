0e832feeb8841581eb5988dea1822e9f68e56784
hoseok <swiftie52@naver.com>
2024-11-07 21:22:40 +0900

    Initial commit

---
+++ src/main/java/com/web/mvc/Security03RegisterApplication.java
@@ -0,0 +1,13 @@
+package com.web.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security03RegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security03RegisterApplication.class, args);
+    }
+
+}
